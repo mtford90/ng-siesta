dec4d99948b997597b10f7f4ebd78859f16e21e3
Michael Ford <mtford@gmail.com>
2015-01-18 10:19:59 +0000

    Bug fixes. oldOn.call was specifying the func as 'this' instead of the events module. We also need to check if a  is already in progress before calling apply. Because of the async nature of events etc, a  may already in progress so this is a perfectly valid check. If  isn't used we catch any errors and pass to angular's own exception handler. I also got rid of the .install call in the service as that isnt neccessary

--- ngSiesta.js
+++ ngSiesta.js
@@ -1,71 +1,55 @@
 'use strict';
 
 angular.module('ngSiesta', [])
-.run(function($window, $q, $rootScope) {
-  $window.q = $q;
+    .run(function ($window, $q, $rootScope, $exceptionHandler) {
+        $window.q = $q;
 
-  $window.siesta.install().then(function() {
-    console.log('installed');
-  });
+        $window.siesta._internal.events.on('siesta', function (e) {
+            console.log(e);
+        });
 
-  /**
-   * Things that don't work for emiting events
-   */
+        $window.siesta.on('siesta', function (e) {
+            console.log(e);
+        });
 
-  // $window.siesta._internal.events.on('siesta', function(e) {
-  //   console.log(e);
-  // });
-
-  // $window.siesta.on('siesta', function(e) {
-  //   console.log(e);
-  // });
-
-  // var oldOn = $window.siesta._internal.events.on;
-  // $window.siesta._internal.events.on = function (event, fn) {
-  //     console.log(event);
-  //     // oldOn.call($window.siesta._internal.events.on, event, function (e) {
-  //     //     $rootScope.$apply(function() {
-  //     //         fn(e);
-  //     //     });
-  //     // });
-      
-  //     /**
-  //      * The following errors out with:
-  //      * Error: [$rootScope:inprog] $apply already in progress
-  //      */
-  //     // $rootScope.$apply(function() {
-  //     //   oldOn.call($window.siesta._internal.events.on, event, function(e) {
-  //     //     fn(e);
-  //     //   });
-  //     // });
-  // };
-})
-.service('Siesta', function($window) {
-  return $window.siesta;
-});
+        var oldOn = $window.siesta._internal.events.on;
+        $window.siesta._internal.events.on = function (event, fn) {
+            oldOn.call($window.siesta._internal.events, event, function (e) {
+                // http://stackoverflow.com/questions/12729122/prevent-error-digest-already-in-progress-when-calling-scope-apply
+                if (!$rootScope.$$phase) {
+                    $rootScope.$apply(function () {
+                        fn(e);
+                    });
+                }
+                else {
+                    try {
+                        fn(e);
+                    }
+                    catch (error) {
+                        $exceptionHandler(error);
+                    }
+                }
+            });
+        };
+    })
+    .service('Siesta', function ($window) {
+        return $window.siesta;
+    });
 
 angular.module('siestaApp', ['ngSiesta'])
-.controller('ctrl', function(Siesta, $rootScope) {
+    .controller('ctrl', function (Siesta, $rootScope) {
+        var C = Siesta.collection('foo'),
+            M = C.model('m', {
+                attributes: ['bar']
+            });
 
+        M.listen(function (e) {
+            console.log(e);
+        });
 
-  var C = Siesta.collection('foo'),
-      M = C.model('m', {
-        attributes: ['bar']
-      });
-
-  C.listen(function(e) {
-    console.log(e);
-  });
-
-  M.listen(function(e) {
-    console.log(e);
-  });
-
-  M.map({
-    bar: 'foo'
-  })
-  .then(function(model) {
-    // console.log(err);
-    model.bar = 'baz';
-  });
-});
+        M.map({
+            bar: 'foo'
+        }).then(function (model) {
+            model.bar = 'baz';
+        });
+    });
